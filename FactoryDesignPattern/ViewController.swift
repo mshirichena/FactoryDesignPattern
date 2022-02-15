fba30c98b3223f1ee0092d4716fc00d6a2e477b5
ChristianShirichena <mshirichena@hotmail.com>
2022-02-14 21:08:24 -0500

    Initial Commit

---
+++ FactoryDesignPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FactoryDesignPattern
+//
+//  Created by Christian Shirichena on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
